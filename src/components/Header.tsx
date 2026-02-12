60fee7ce53910c1959bebc9c04d889b3fdda0b55
HannanAmar05 <amarhannan358@gmail.com>
2026-02-12 21:57:19 +0700

    yesss

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,6 @@ const Header = () => {
       <h1 className="text-2xl font-black text-[#1E3A8A]">
         Food<span className="text-[#3B82F6]">Explore.</span>
       </h1>
-      <div className="flex items-center gap-6">
-        <button className="text-gray-600 font-medium">Sign In</button>
-        <button className="bg-[#3B82F6] text-white px-6 py-2 rounded-lg font-bold shadow-md hover:bg-blue-700 transition">
-          Register
-        </button>
-      </div>
     </nav>
   );
 };
